9f020548ef874979a888021f656046650533733c
Ebicoglu <alper.ebicoglu@volosoft.com>
2022-06-15 14:26:11 +0300

    Update microservice-subservice.ps1

--- commercial/microservice-subservice.ps1
+++ commercial/microservice-subservice.ps1
@@ -1,3 +1,3 @@
 # this command should be run inside a folder that contains a microservice solution!
 if ((Test-Path -Path "_common.ps1") -eq $True) { . ./_common.ps1}
-iex "abp new Microservice.SubService$unique_number -t microservice-service-pro -u mvc --mobile none --database-provider ef -csf $output_folder"
+iex "abp new Microservice.SubService$unique_number -t microservice-service-pro"
